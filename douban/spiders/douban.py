ffe291878d4aaf0e3f23f47e45b0b54803e52df0
chenyl <chenyl@datatowisdom.cn>
2019-05-31 10:04:39 +0800

    更加精准爬取历史标签书籍

--- douban/spiders/douban.py
+++ douban/spiders/douban.py
@@ -7,18 +7,18 @@ from douban.items import DoubanItem
 class doubanSpider(scrapy.Spider):
     name = "douban"
     allowed_domains = ["douban.com"]
-    start_urls = ['https://www.douban.com/tag/%E7%A4%BE%E7%A7%91/book']
+    start_urls = ['https://book.douban.com/tag/%E5%8E%86%E5%8F%B2']
 
     def parse(self, response):  # 提取数据到Items里面，主要用到XPath和CSS选择器提取网页数据
         item = DoubanItem()
         selector = Selector(response)
-        Book = selector.xpath('//div[@class="article"]')
+        Book = selector.xpath('//div[@class="info"]')
         Books = selector.css('dl dd')
-        for book in Books:
-            item['name'] = book.css('a').xpath('text()').extract()
-            item['dataLink'] = book.css('a').xpath('@href').extract()
-            item['author'] = book.xpath('div[@class="desc"]/text()').extract()
-            item['grade'] = book.xpath('div[@class="rating"]/span/text()').extract()
+        for book in Book:
+            item['name'] = book.xpath('h2/a/text()').extract()
+            item['dataLink'] = book.xpath('h2/a/@href').extract()
+            item['author'] = book.xpath('div[@class="pub"]/text()').extract()
+            item['grade'] = book.xpath('div[@class="star clearfix"]/span/text()').extract()
             yield item
         nextLink = selector.xpath('//span[@class="next"]/link/@href').extract()
         # 第10页是最后一页，没有下一页的链接
